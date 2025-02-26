6c370cd5e73886112dc1533b77ad90bada00d757
Khin <khinthirikyaw8@gmail.com>
2025-02-26 18:57:46 +0700

    - Added , , , and  functions for spreadsheet calculations. - Defined  object to group utility functions. - Implemented  and  functions for generating row/column labels. - Introduced  with regex support for parsing cell ranges. - Enhanced input handling in  function with validation for formulas. - Improved spreadsheet UI with dynamic input fields and labels.

--- Spreadsheet/spreadsheet.js
+++ Spreadsheet/spreadsheet.js
@@ -1,5 +1,23 @@
+const isEven = (num) => (num % 2 === 0 ? true : false);
+const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
+const average = (nums) => sum(nums) / nums.length;
+const median = (nums) => {
+  const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = Math.floor(length / 2 - 1);
+  return isEven(length)
+    ? average([sorted[middle - 1], sorted[middle]])
+    : sorted[middle];
+};
+
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median,
+};
+
 const range = (start, end) =>
-  Array(end - start - 1)
+  Array(end - start + 1)
     .fill(start)
     .map((element, index) => element + index);
 
@@ -7,6 +25,11 @@ const charRange = (start, end) =>
   range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
     String.fromCharCode(code)
   );
+
+const evalFormula = (x, cells) => {
+  const idToText = (id) => cells.find((cell) => cell.id === id).value;
+  const rangeTegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+};
 
 window.onload = () => {
   const container = document.getElementById("container");
@@ -16,4 +39,24 @@ window.onload = () => {
     label.textContent = name;
     container.appendChild(label);
   };
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach((number) => {
+    createLabel(number);
+    letters.forEach((letter) => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = input.id;
+      input.onchange = update;
+      container.appendChild(input);
+    });
+  });
+};
+
+const update = (event) => {
+  const element = event.target;
+  const value = element.value.replace(/\s/g, "");
+  if (!value.includes(element.id) && value.startsWith("=")) {
+  }
 };

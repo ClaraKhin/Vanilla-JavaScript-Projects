a250b247ce50585b014f9043dd7f15878cacbae6
Khin <khinthirikyaw8@gmail.com>
2025-03-03 19:59:28 +0700

    feat(cart): Add shopping cart class and product addition functionality feat: Display product list dynamically and enable cart item addition feat: Implement cart toggle and dynamic product rendering

--- Shopping-Cart/cart.js
+++ Shopping-Cart/cart.js
@@ -87,3 +87,57 @@ const products = [
     category: "Cupcake",
   },
 ];
+
+products.forEach(({ name, id, price, category }) => {
+  dessertCards.innerHTML += `
+  <div class="dessert-card">
+  <h2>${name}</h2>
+  <p class="dessert-price">$${price}</p>
+  <p class="product-category">Category: ${category}</p>
+  <button id="${id}" class="add-to-cart-btn">Add to cart</button>
+  </div>
+  `;
+});
+
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+    this.total = 0;
+    this.taxRate = 8.25;
+  }
+  addItem(id, products) {
+    const product = products.find((item) => item.id === id);
+    const { name, price } = product;
+    this.items.push(product);
+    const totalCountPerProduct = {};
+    this.items.forEach((dessert) => {
+      totalCountPerProduct[dessert.id] =
+        (totalCountPerProduct[dessert.id] || 0) + 1;
+    });
+    const currentProductCount = totalCountPerProduct[product.id];
+    const currentProductCountSpan = document.getElementById(
+      `product-count-for-id${product.id}`
+    );
+    currentProductCount > 1
+      ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
+      : (productsContainer.innerHTML += `<div class="product" id="dessert${id}">
+        <p>
+        <span class="product-count" id="product-count-for-id${id}"></span> ${name}
+        </p>
+        <p>${price}</p>
+        </div>`);
+  }
+}
+
+const cart = new ShoppingCart();
+
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+[...addToCartBtns].forEach((btn) =>
+  btn.addEventListener("click", (event) => {
+    cart.addItem(Number(event.target.id), products);
+  })
+);
+
+cartBtn.addEventListener("click", () => {
+  isCartShowing = !isCartShowing;
+});

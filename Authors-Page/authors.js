ada72ff496a4865149cb4adbb4a84a5f821b666f
Khin <khinthirikyaw8@gmail.com>
2025-03-09 16:18:43 +0700

    - Fetch authors from Gutendex API and display them dynamically - Implement error handling with a fallback message - Add Read link to access the book in plain text format - Limit summaries to 200 characters with truncation - Disable 'Load More' button when no more data is available

--- Authors-Page/authors.js
+++ Authors-Page/authors.js
@@ -14,12 +14,19 @@ fetch("https://gutendex.com/books/")
   })
   .catch((err) => {
     console.error(`There was an error: ${err}`);
+    authorContainer.innerHTML = `<p class="error-msg">There was an error loading the authors!</p>`;
   });
 
 const fetchMoreAuthors = () => {
   startingIndex += 10;
   endingIndex += 10;
   displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+
+  if (authorDataArr.length <= endingIndex) {
+    loadMoreBtn.disbaled = true;
+    loadMoreBtn.style.cursor = "not-allowed";
+    loadMoreBtn.textContent = "No more data to load!";
+  }
 };
 
 const displayAuthors = (books) => {
@@ -27,9 +34,17 @@ const displayAuthors = (books) => {
     authorContainer.innerHTML += `
     <div id="${index}" class="user-card">
 <h2 class="author-name">${authors[0].name}</h2>
-<img class="user-img" src="${formats["image/jpeg"]}" alt="${authors[0].name} cover" />
+<img class="user-img" src="${formats["image/jpeg"]}" alt="${
+      authors[0].name
+    } cover" />
+<div class="purple-divider"></div>
 <p class="title">${title}<p>
-<p class="summaries">${summaries}</p>
+<p class="summaries">${
+      summaries.length > 200 ? summaries.slice(0, 200) + "..." : summaries
+    }</p>
+    <a class="book-link" href="${
+      formats["text/plain; charset=us-ascii"]
+    }" target="_blank">Read</a>
     </div>
   `;
   });
